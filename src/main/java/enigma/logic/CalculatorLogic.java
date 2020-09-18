620f458bdd772e45d3e1ea4830f0ab5d97bd99c8
Csaba Szegedi <dev.csaba.szegedi@gmail.com>
2020-09-18 22:04:32 +0200

    Added support for storing calculator states and previous information in operator logic

--- src/main/java/enigma/logic/CalculatorLogic.java
+++ src/main/java/enigma/logic/CalculatorLogic.java
@@ -2,6 +2,35 @@ package enigma.logic;
 
 public class CalculatorLogic {
 
+    public enum CalculatorOperator {
+        NONE, ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION, POWER, PERCENTAGE
+    }
+
+    private CalculatorOperator storedOperator;
+    private double storedValue;
+
+    public CalculatorLogic() {
+        storedOperator = CalculatorOperator.NONE;
+        storedValue = 0;
+    }
+
+
+    public CalculatorOperator getStoredOperator() {
+        return storedOperator;
+    }
+
+    public double getStoredValue() {
+        return storedValue;
+    }
+
+    public void setStoredOperator(CalculatorOperator storedOperator) {
+        this.storedOperator = storedOperator;
+    }
+
+    public void setStoredValue(double storedValue) {
+        this.storedValue = storedValue;
+    }
+
     public double additionOperator(double x, double y) {
 
         return x + y;
