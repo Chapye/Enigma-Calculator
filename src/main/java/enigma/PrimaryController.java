e96e65c1949e2f8bee38d4f1f3d68a5d02f756c3
norbertHarmati <norbert.harmati92@gmail.com>
2020-09-19 15:16:33 +0200

    Implementation of button method frame

--- src/main/java/enigma/PrimaryController.java
+++ src/main/java/enigma/PrimaryController.java
@@ -9,4 +9,44 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    @FXML
+    public void processDigit(){}
+
+    @FXML
+    public void processEqual(){}
+
+    @FXML
+    public void processAddition() {}
+
+    @FXML
+    public void processSubtraction() {}
+
+    @FXML
+    public void processMultiplication() {}
+
+    @FXML
+    public void processDivision() {}
+
+    @FXML
+    public void processExponentiation() {}
+
+    @FXML
+    public void processPercentage() {}
+
+    @FXML
+    public void processNegate() {}
+
+    @FXML
+    public void processDecimal() {}
+
+    @FXML
+    public void processClear() {}
+
+    @FXML
+    public void processAllClear() {}
+
+    @FXML
+    public void processBackspace() {}
+
+    
 }
