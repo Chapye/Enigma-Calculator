9165e815bc3eb26d16346eebad4372213f3079d9
Csaba Szegedi <chapye@mailbox.unideb.hu>
2020-09-21 15:01:08 +0200

    Added keyboard support, and removed unused Stage

--- src/main/java/enigma/PrimaryController.java
+++ src/main/java/enigma/PrimaryController.java
@@ -8,20 +8,23 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+@SuppressWarnings("all") // For VScode users to disable transitive dep warnings
 public class PrimaryController {
     @FXML
     public TextField MainDisplay;
     public TextField MiniDisplay;
     public Button EqualsButton;
     CalculatorLogic logic = new CalculatorLogic();
+
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        EnigmaCalculatorMain.setRoot("secondary");
     }
+
     @FXML
     public void processDigit(Event buttonevent) {
         if (MainDisplay.getText().equals("NaN") || MainDisplay.getText().equals("Infinity")
-                || MainDisplay.getText().equals("0") ) {
+                || MainDisplay.getText().equals("0")) {
             MainDisplay.setText("");
             Object node = buttonevent.getSource();
             Button b = (Button) node;
@@ -31,65 +34,89 @@ public class PrimaryController {
             Object node = buttonevent.getSource();
             Button b = (Button) node;
             MainDisplay.setText(MainDisplay.getText() + b.getText());
-        }}
+        }
+    }
 
     @FXML
-    public void processEqual(){}
+    public void processDigit(char digit) {
+
+        if (MainDisplay.getText().equals("NaN") || MainDisplay.getText().equals("Infinity")
+                || MainDisplay.getText().equals("0")) {
+            MainDisplay.setText("");
+            MainDisplay.setText("" + digit);
+        } else {
+            MainDisplay.setText(MainDisplay.getText() + digit);
+        }
+    }
 
     @FXML
-    public void processAddition() {}
+    public void processEqual() {
+    }
 
     @FXML
-    public void processSubtraction() {}
+    public void processAddition() {
+    }
 
     @FXML
-    public void processMultiplication() {}
+    public void processSubtraction() {
+    }
 
     @FXML
-    public void processDivision() {}
+    public void processMultiplication() {
+    }
 
     @FXML
-    public void processExponentiation() {}
+    public void processDivision() {
+    }
 
     @FXML
-    public void processPercentage() {}
+    public void processExponentiation() {
+    }
 
     @FXML
-    public void processNegate() {}
+    public void processPercentage() {
+    }
 
     @FXML
-    public void processDecimal() {}
+    public void processNegate() {
+    }
 
     @FXML
-    public void processClear() {setDisplay(MainDisplay,0);}
+    public void processDecimal() {
+    }
+
+    @FXML
+    public void processClear() {
+        setDisplay(MainDisplay, 0);
+    }
 
     @FXML
     public void processAllClear() {
         logic.setStoredValue(0.0);
         logic.setStoredOperator(CalculatorLogic.CalculatorOperator.NONE);
-        setDisplay(MainDisplay,0);
-        setDisplay(MiniDisplay,"");
+        setDisplay(MainDisplay, 0);
+        setDisplay(MiniDisplay, "");
         MainDisplay.setStyle("-fx-font: 36 System");
     }
 
     @FXML
-    public void processBackspace() {}
-
-    private void setDisplay(TextField mainDisplay, double storedValue, String s){
-        if((int)storedValue==storedValue)
-            mainDisplay.setText(String.valueOf((int)storedValue)+" "+s);
-        else
-            mainDisplay.setText(String.valueOf(storedValue)+" "+s);
+    public void processBackspace() {
     }
 
+    private void setDisplay(TextField mainDisplay, double storedValue, String s) {
+        if ((int) storedValue == storedValue)
+            mainDisplay.setText(String.valueOf((int) storedValue) + " " + s);
+        else
+            mainDisplay.setText(String.valueOf(storedValue) + " " + s);
+    }
 
-    private void setDisplay(TextField display,String message){
+    private void setDisplay(TextField display, String message) {
         display.setText(message);
     }
 
-    private  void setDisplay(TextField mainDisplay, double storedValue){
-        if((int)storedValue==storedValue)
-            mainDisplay.setText(String.valueOf((int)storedValue));
+    private void setDisplay(TextField mainDisplay, double storedValue) {
+        if ((int) storedValue == storedValue)
+            mainDisplay.setText(String.valueOf((int) storedValue));
         else
             mainDisplay.setText(String.valueOf(storedValue));
     }
