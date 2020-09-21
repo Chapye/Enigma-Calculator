e6131dfb25855faff19bb55963f3b447f378f1c1
GSG9HUN <43461831+GSG9HUN@users.noreply.github.com>
2020-09-21 10:39:50 +0200

    setDisplay methods created and implemented

--- src/main/java/enigma/PrimaryController.java
+++ src/main/java/enigma/PrimaryController.java
@@ -2,6 +2,7 @@ package enigma;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
@@ -48,5 +49,22 @@ public class PrimaryController {
     @FXML
     public void processBackspace() {}
 
-    
+    private void setDisplay(TextField mainDisplay, double storedValue, String s){
+        if((int)storedValue==storedValue)
+            mainDisplay.setText(String.valueOf((int)storedValue)+" "+s);
+        else
+            mainDisplay.setText(String.valueOf(storedValue)+" "+s);
+    }
+
+
+    private void setDisplay(TextField display,String message){
+        display.setText(message);
+    }
+
+    private  void setDisplay(TextField mainDisplay, double storedValue){
+        if((int)storedValue==storedValue)
+            mainDisplay.setText(String.valueOf((int)storedValue));
+        else
+            mainDisplay.setText(String.valueOf(storedValue));
+    }
 }
